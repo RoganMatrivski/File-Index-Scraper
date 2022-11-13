9d751368904079991d57c53721cef4410a72d2f4
Robin Mauritz <robinmauritzrm@gmail.com>
2022-11-14 00:03:00 +0700

    feat: Add logging to built-in traits

--- src/simple_file_info.rs
+++ src/simple_file_info.rs
@@ -5,14 +5,17 @@ pub struct SimpleFileInfo {
 }
 
 impl SimpleFileInfo {
+    #[tracing::instrument(skip(self))]
     pub fn get_full_path(&self) -> String {
         format!("{}{}", self.dir, self.file)
     }
 
+    #[tracing::instrument(skip(self))]
     pub fn get_url_relative_path(&self) -> String {
         self.get_full_path()[2..].to_string()
     }
 
+    #[tracing::instrument(skip(self))]
     pub fn get_decoded_full_path(&self) -> String {
         urlencoding::decode(&self.get_full_path())
             .unwrap()
