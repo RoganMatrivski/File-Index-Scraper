ce9d424de62652b806841545adc962910714da4e
Robin Mauritz <robinmauritzrm@gmail.com>
2023-02-03 23:22:12 +0000

    refactor: Change how url query string fetch works
    Probably will only make about 10ms improvements, but eh idgaf
    Based on benchmarks i do on my own Rust benchmark repo

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,11 @@ async fn main() -> anyhow::Result<()> {
 
     let url = args.url;
     let base = args.base_path + "/";
-    let mut split_url = url.split('?');
-    let url = split_url.next().unwrap_or(&url).to_string();
-    let url_query = ("?".to_string() + split_url.next().unwrap_or("")).to_string();
-    let res = walker_async(&url, &url_query, "".to_string()).await?;
+
+    // Get the position of the question mark if exist, else just return string length
+    let url_query_startpos = url.chars().position(|x| x == '?').unwrap_or(url.len());
+    let (url, url_query) = url.split_at(url_query_startpos);
+    let res = walker_async(url, url_query, "".to_string()).await?;
 
     match args.format {
         crate::enums::FormatArgs::PlainText => {
@@ -27,7 +28,7 @@ async fn main() -> anyhow::Result<()> {
             for link in res {
                 println!(
                     "{}\n    out={}",
-                    url.clone() + &link.get_full_path() + &url_query,
+                    url.to_owned() + &link.get_full_path() + url_query,
                     base.to_owned() + &link.get_decoded_full_path()
                 );
             }
